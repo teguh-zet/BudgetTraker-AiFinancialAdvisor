50c0ff7d818bb59a537041531ef9374c7fce98fa
teguh ahmadi zebua <zetteaz@gmail.com>
2025-05-31 13:32:04 +0700

    update mothly summary

---
+++ src/modules/monthlySummary/monthlySummary.model.js
@@ -0,0 +1,55 @@
+module.exports = (Sequelize, DataTypes) => {
+    const MonthlySummary = Sequelize.define("MonthlySummary", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        month: {
+            type: DataTypes.STRING(25),
+            allowNull: false
+        },
+        year: {
+            type: DataTypes.STRING(4),
+            allowNull: false
+        },
+        total_income: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        total_expense: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        balance: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ai_summary: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        ai_recomendation: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    }, {
+        Sequelize,
+        modelName: 'MonthlySummary',
+        tableName: 'monthly_summaries',
+        timestamps: true,
+        underscored: true
+    });
+    MonthlySummary.associate = (models) => {
+        MonthlySummary.belongsTo(models.User, {
+            foreignKey: 'user_id',
+            as: 'summary_user'
+        });
+    };
+
+    return MonthlySummary;
+}
